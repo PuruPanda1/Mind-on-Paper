64dc8c7f2b1565036dcd70d0c2c3d52f79f9333b
PuruPanda1 <purabmodi2002@gmail.com>
2025-09-12 15:41:48 +0530

    Initial Commit

---
+++ src/main/java/com/encora/purab/mind_on_paper/MindOnPaperApplication.java
@@ -0,0 +1,13 @@
+package com.encora.purab.mind_on_paper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindOnPaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindOnPaperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encora/purab/mind_on_paper/MindOnPaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encora.purab.mind_on_paper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindOnPaperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
